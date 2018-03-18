536b8d82e1777f14e946d509acd22b9a7523ba49
sonhoai272@gmail.com <sonhoai272@gmail.com>
2018-03-18 11:11:15 +0700

    a

--- app/src/main/java/com/sonhoai/sonho/lab5/B1/B2/AddUpdate.java
+++ app/src/main/java/com/sonhoai/sonho/lab5/B1/B2/AddUpdate.java
@@ -1,20 +1,32 @@
 package com.sonhoai.sonho.lab5.B1.B2;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.sonhoai.sonho.lab5.B1.B1_todolist;
 import com.sonhoai.sonho.lab5.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class AddUpdate extends AppCompatActivity {
     private Button btnAdd;
-    private EditText edtFirst, edtLast, edtGender, edtHire, edtDept;
+    private EditText edtFirst, edtLast, edtHire, edtDept;
+    private RadioGroup edtGender;
     private String ID = null;
+    private RadioButton btnGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,7 @@ public class AddUpdate extends AppCompatActivity {
         edtHire = findViewById(R.id.edtHire);
         edtDept = findViewById(R.id.edtDept);
         btnAdd = findViewById(R.id.btnAdd2);
+        chooseBirthday();
 
         Bundle bundle = getIntent().getExtras();
         if(bundle.getString("ID").equals("ADD")){
@@ -35,7 +48,6 @@ public class AddUpdate extends AppCompatActivity {
             ID = "EDIT";
             edtFirst.setText(bundle.getString("First"));
             edtLast.setText(bundle.getString("LastName"));
-            edtGender.setText(bundle.getString("Gender"));
             edtHire.setText(bundle.getString("Hire"));
             edtDept.setText(bundle.getString("Dept"));
         }
@@ -43,14 +55,45 @@ public class AddUpdate extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(AddUpdate.this, B2.class);
+                int selectedId = edtGender.getCheckedRadioButtonId();
+                // find the radiobutton by returned id
+                btnGender = findViewById(selectedId);
                 intent.putExtra("ID", ID);
                 intent.putExtra("First", edtFirst.getText().toString());
                 intent.putExtra("LastName", edtLast.getText().toString());
-                intent.putExtra("Gender", edtGender.getText().toString());
+                intent.putExtra("Gender",  btnGender.getText().toString());
                 intent.putExtra("Hire", edtHire.getText().toString());
                 intent.putExtra("Dept", edtDept.getText().toString());
                 setResult(1333, intent);
                 finish();
+            }
+        });
+    }
+
+    private void chooseBirthday(){
+        final Calendar calendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, day);
+                String calenderFormat = "yyyy-MM-dd";
+                SimpleDateFormat sdf = new SimpleDateFormat(calenderFormat, Locale.US);
+                edtHire.setText(sdf.format(calendar.getTime()));
+            }
+        };
+        edtHire.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(
+                        view.getContext(),
+                        date,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
             }
         });
     }
